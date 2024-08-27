49fd13d40d0c06990193db565a36e4a07383b6a6
Nihara Nethmini <niharanethmini26@gmail.com>
2024-08-27 23:55:38 +0530

    Connected the database
    Created the database
    Completed Add button

--- GrifindoLeaveMS/GrifindoLeaveMS/Add_Emp.Designer.cs
+++ GrifindoLeaveMS/GrifindoLeaveMS/Add_Emp.Designer.cs
@@ -28,11 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Add_Emp));
             this.label1 = new System.Windows.Forms.Label();
             this.txtname = new System.Windows.Forms.TextBox();
             this.txtpw = new System.Windows.Forms.TextBox();
-            this.txtempno = new System.Windows.Forms.TextBox();
+            this.txtEmpID = new System.Windows.Forms.TextBox();
             this.datePickerDOB = new System.Windows.Forms.DateTimePicker();
             this.txtaddress = new System.Windows.Forms.TextBox();
             this.datePickerJobDate = new System.Windows.Forms.DateTimePicker();
@@ -49,7 +50,19 @@
             this.dataGridAddEmp = new System.Windows.Forms.DataGridView();
             this.btnBack = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.grifindoLeaveMSDataSet2 = new GrifindoLeaveMS.GrifindoLeaveMSDataSet2();
+            this.employeeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.employeeTableAdapter = new GrifindoLeaveMS.GrifindoLeaveMSDataSet2TableAdapters.EmployeeTableAdapter();
+            this.employeeIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.passwordDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dOBDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phoneNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.jobStartDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridAddEmp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grifindoLeaveMSDataSet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -81,14 +94,14 @@
             this.txtpw.Size = new System.Drawing.Size(229, 27);
             this.txtpw.TabIndex = 15;
             // 
-            // txtempno
+            // txtEmpID
             // 
-            this.txtempno.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtempno.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.txtempno.Location = new System.Drawing.Point(411, 152);
-            this.txtempno.Name = "txtempno";
-            this.txtempno.Size = new System.Drawing.Size(229, 27);
-            this.txtempno.TabIndex = 16;
+            this.txtEmpID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmpID.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtEmpID.Location = new System.Drawing.Point(411, 152);
+            this.txtEmpID.Name = "txtEmpID";
+            this.txtEmpID.Size = new System.Drawing.Size(229, 27);
+            this.txtEmpID.TabIndex = 16;
             // 
             // datePickerDOB
             // 
@@ -133,6 +146,7 @@
             this.btnAdd.TabIndex = 21;
             this.btnAdd.Text = "Add ";
             this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // label4
             // 
@@ -152,9 +166,9 @@
             this.label2.Location = new System.Drawing.Point(402, 129);
             this.label2.Margin = new System.Windows.Forms.Padding(0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 20);
+            this.label2.Size = new System.Drawing.Size(100, 20);
             this.label2.TabIndex = 23;
-            this.label2.Text = "Employee No:";
+            this.label2.Text = "EmployeeID:";
             // 
             // label3
             // 
@@ -225,9 +239,19 @@
             // 
             // dataGridAddEmp
             // 
+            this.dataGridAddEmp.AutoGenerateColumns = false;
             this.dataGridAddEmp.BackgroundColor = System.Drawing.SystemColors.ControlLight;
             this.dataGridAddEmp.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dataGridAddEmp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridAddEmp.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.employeeIDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.passwordDataGridViewTextBoxColumn,
+            this.dOBDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn,
+            this.phoneNoDataGridViewTextBoxColumn,
+            this.jobStartDateDataGridViewTextBoxColumn});
+            this.dataGridAddEmp.DataSource = this.employeeBindingSource;
             this.dataGridAddEmp.Location = new System.Drawing.Point(84, 411);
             this.dataGridAddEmp.Name = "dataGridAddEmp";
             this.dataGridAddEmp.RowHeadersWidth = 51;
@@ -261,6 +285,77 @@
             this.button1.Text = "View All";
             this.button1.UseVisualStyleBackColor = false;
             // 
+            // grifindoLeaveMSDataSet2
+            // 
+            this.grifindoLeaveMSDataSet2.DataSetName = "GrifindoLeaveMSDataSet2";
+            this.grifindoLeaveMSDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employeeBindingSource
+            // 
+            this.employeeBindingSource.DataMember = "Employee";
+            this.employeeBindingSource.DataSource = this.grifindoLeaveMSDataSet2;
+            // 
+            // employeeTableAdapter
+            // 
+            this.employeeTableAdapter.ClearBeforeFill = true;
+            // 
+            // employeeIDDataGridViewTextBoxColumn
+            // 
+            this.employeeIDDataGridViewTextBoxColumn.DataPropertyName = "EmployeeID";
+            this.employeeIDDataGridViewTextBoxColumn.HeaderText = "EmployeeID";
+            this.employeeIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.employeeIDDataGridViewTextBoxColumn.Name = "employeeIDDataGridViewTextBoxColumn";
+            this.employeeIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.employeeIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // passwordDataGridViewTextBoxColumn
+            // 
+            this.passwordDataGridViewTextBoxColumn.DataPropertyName = "Password";
+            this.passwordDataGridViewTextBoxColumn.HeaderText = "Password";
+            this.passwordDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.passwordDataGridViewTextBoxColumn.Name = "passwordDataGridViewTextBoxColumn";
+            this.passwordDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // dOBDataGridViewTextBoxColumn
+            // 
+            this.dOBDataGridViewTextBoxColumn.DataPropertyName = "DOB";
+            this.dOBDataGridViewTextBoxColumn.HeaderText = "DOB";
+            this.dOBDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.dOBDataGridViewTextBoxColumn.Name = "dOBDataGridViewTextBoxColumn";
+            this.dOBDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "Address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "Address";
+            this.addressDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            this.addressDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // phoneNoDataGridViewTextBoxColumn
+            // 
+            this.phoneNoDataGridViewTextBoxColumn.DataPropertyName = "PhoneNo";
+            this.phoneNoDataGridViewTextBoxColumn.HeaderText = "PhoneNo";
+            this.phoneNoDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.phoneNoDataGridViewTextBoxColumn.Name = "phoneNoDataGridViewTextBoxColumn";
+            this.phoneNoDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // jobStartDateDataGridViewTextBoxColumn
+            // 
+            this.jobStartDateDataGridViewTextBoxColumn.DataPropertyName = "JobStartDate";
+            this.jobStartDateDataGridViewTextBoxColumn.HeaderText = "JobStartDate";
+            this.jobStartDateDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.jobStartDateDataGridViewTextBoxColumn.Name = "jobStartDateDataGridViewTextBoxColumn";
+            this.jobStartDateDataGridViewTextBoxColumn.Width = 125;
+            // 
             // Add_Emp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -283,13 +378,16 @@
             this.Controls.Add(this.datePickerJobDate);
             this.Controls.Add(this.txtaddress);
             this.Controls.Add(this.datePickerDOB);
-            this.Controls.Add(this.txtempno);
+            this.Controls.Add(this.txtEmpID);
             this.Controls.Add(this.txtpw);
             this.Controls.Add(this.txtname);
             this.Controls.Add(this.label1);
             this.Name = "Add_Emp";
             this.Text = "Add_Emp";
+            this.Load += new System.EventHandler(this.Add_Emp_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridAddEmp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grifindoLeaveMSDataSet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -300,7 +398,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtname;
         private System.Windows.Forms.TextBox txtpw;
-        private System.Windows.Forms.TextBox txtempno;
+        private System.Windows.Forms.TextBox txtEmpID;
         private System.Windows.Forms.DateTimePicker datePickerDOB;
         private System.Windows.Forms.TextBox txtaddress;
         private System.Windows.Forms.DateTimePicker datePickerJobDate;
@@ -317,5 +415,15 @@
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.DataGridView dataGridAddEmp;
         private System.Windows.Forms.Button button1;
+        private GrifindoLeaveMSDataSet2 grifindoLeaveMSDataSet2;
+        private System.Windows.Forms.BindingSource employeeBindingSource;
+        private GrifindoLeaveMSDataSet2TableAdapters.EmployeeTableAdapter employeeTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn employeeIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn passwordDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dOBDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phoneNoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn jobStartDateDataGridViewTextBoxColumn;
     }
 }
